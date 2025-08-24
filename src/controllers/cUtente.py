b9799213271b219a804d75da24f07e144c1731e6
Davidoscki <davidoscki@hotmail.com>
2025-08-24 15:31:34 +0200

    aggiunte update super gubbato 100/100

--- src/controllers/cUtente.py
+++ src/controllers/cUtente.py
@@ -4,9 +4,41 @@ import psycopg2.extras
 from servizi.servizioDB import connetti_db
 
 def update(userID):
-    # TODO : update; tuttoh
-    # 
-    pass
+    data = request.get_json()
+    username = data.get('username')
+    telefono = data.get('telefono')
+    nome = data.get('nome')
+    cognome = data.get('cognome')
+
+    try :
+        conn = connetti_db()
+        cur = conn.cursor()
+
+        cur.execute("SELECT id FROM users WHERE username = %s;", (username,))
+        if cur.fetchone():
+            return jsonify({'error': 'Username già esistente'}), 409
+
+
+        cur.execute("UPDATE users SET username = %s,telefono = %s,nome = %s,cognome = %s WHERE id = %s", (username, telefono, nome,cognome, userID,))
+        userData = cur.fetchone()
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        if userData:
+            return jsonify({
+                'username': username,
+                'telefono': telefono,
+                'nome': nome,
+                'cognome': cognome
+            }), 200
+        else:
+            return jsonify({'message': 'nessun utente trovato'}), 404
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        traceback.print_exc()
+        return jsonify({'error': 'Internal server error'}), 500
 
 def eliminaUtente(userID):
     try :
