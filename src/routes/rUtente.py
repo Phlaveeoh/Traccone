7d405dc10cf0c9cbe1e733054a544724a160428e
Phlaveeoh <flavio.merli.tv.03@gmail.com>
2025-08-25 19:50:49 +0200

    Sistemato handler su cUtente.py

--- src/routes/rUtente.py
+++ src/routes/rUtente.py
@@ -8,7 +8,7 @@ from servizi.servizioAutenticatore import valida_token
 utente_bp = Blueprint('utenti', __name__)
 
 
-#/api/posizioni/save 
+#/api/utenti/<user_id>/update
 @utente_bp.route('/<int:user_id>/update', methods=['PATCH'])
 @valida_token
 def handle_update(current_user_id, user_id):
@@ -16,6 +16,7 @@ def handle_update(current_user_id, user_id):
         return jsonify({"errore": "Non sei autorizzato ad accedere a questo contenuto"}), 403
     return updateUtente(user_id)
 
+#/api/utenti/<user_id>/cambiaPassword
 @utente_bp.route('/<int:user_id>/cambiaPassword', methods=['PATCH'])
 @valida_token
 def handle_change_password(current_user_id, user_id):
@@ -23,6 +24,7 @@ def handle_change_password(current_user_id, user_id):
         return jsonify({"errore": "Non sei autorizzato ad accedere a questo contenuto"}), 403
     return cambiaPassword(user_id)
 
+#/api/utenti/<user_id>/delete
 @utente_bp.route('/<int:user_id>/delete', methods=['DELETE'])
 @valida_token
 def handle_delete(current_user_id, user_id):
@@ -30,13 +32,7 @@ def handle_delete(current_user_id, user_id):
         return jsonify({"errore": "Non sei autorizzato ad accedere a questo contenuto"}), 403
     return deleteUtente(user_id)
 
-@utente_bp.route('/<int:user_id>', methods=['GET'])
-@valida_token
-def handle_visualization(current_user_id, user_id):
-    if current_user_id != user_id:
-        return jsonify({"errore": "Non sei autorizzato ad accedere a questo contenuto"}), 403
-    return getUtente(user_id)
-
+#/api/utenti/<user_id>
 @utente_bp.route('/<int:user_id>', methods=['GET'])
 @valida_token
 def handle_visualization(current_user_id, user_id):
